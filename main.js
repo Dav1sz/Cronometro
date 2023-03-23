121a48b37f4a45025fa52ab5f7d32096c3b556da
Davi.san <106943270+Dav1sz@users.noreply.github.com>
2023-03-23 13:46:10 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,70 @@
+window.onload = function () {
+    //Executa um JS imediatamente após o carregamento da página.
+
+    var seconds = 00;
+    var tens = 00;
+
+    var appendTens = document.getElementById("tens");
+    var appendSeconds = document.getElementById("seconds");
+
+    var buttonStart = document.getElementById("button-start");
+    var buttonStop = document.getElementById("button-stop");
+    var buttonReset = document.getElementById("button-reset");
+
+    var interval;
+
+    buttonStart.onclick = function () {
+        clearInterval(interval) //O método clearInterval() limpa o temporizador defido como método setInterval().
+
+        interval = setInterval(startTimer, 10);
+        //O método setInterval chama uma função em intervalos em valores específicos (milissegundo).
+
+    }
+
+    buttonStop.onclick = function () {
+        clearInterval(interval);
+    }
+
+    buttonReset.onclick = function () {
+        clearInterval(interval);
+        tens = 00;
+        seconds = 00;
+        appendTens.innerHTML = tens;
+        appendSeconds.innerHTML = seconds;
+
+    }
+
+    function startTimer() {
+        tens++ //tens++ pós incremento - ++tens pré incremento.
+
+        if (tens <= 9) {
+            appendTens.innerHTML = "0" + tens;
+        }
+
+        if (tens > 9) {
+            appendTens.innerHTML = tens;
+        }
+
+        if (tens > 99) {
+            console.log("seconds");
+
+            seconds++
+
+            appendSeconds.innerHTML = "0" + seconds;
+
+            tens = 0;
+
+            appendTens.innerHTML = "0" + 0;
+        }
+
+        if (seconds > 9) {
+            appendSeconds.innerHTML = seconds
+        }
+
+    }
+
+}
+
+
+
+
